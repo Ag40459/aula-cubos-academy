15805b086801bc45398751fdb19954dce2fdd1d9
AgenorTorres <96009655+Ag40459@users.noreply.github.com>
2022-03-10 11:08:41 +0000

    Initial commit

---
+++ Entrada de Cinema.js
@@ -0,0 +1,8 @@
+// Criar ap que mostre se a pessoa pode entrar de acordo com idade e com carteira de estudante
+// MENOR DE 18 = MEIA ENTRADA
+// DE 18 A 60 = MEIA ENTRADA APENAS COM CARTEIRINHA
+// MAIOR DE 60 MEIA ENTRADA
+
+const idade = 15;
+const temcarteirinha = false; 
+
---
+++ index.js
@@ -0,0 +1,30 @@
+// Criar ap que mostre o nome digitado pelo operador e se tem autorização para entrar de acodo com a idade
+
+// capturar a digitação do operador
+//let entnome = scanner.next ;// codgo para gravar tecla digitada
+
+let entnome = "José";// codgo para gravar tecla digitada
+let nome = entnome;
+console.log (nome)
+let idade = 65;// codgo para gravar tecla digitada
+
+if (idade < 18) { 
+  console.log ('NÃO PODE ENTRAR SEM ACOMPANHANTE');
+} else if (idade <= 60){
+  console.log ("PODE ENTRAR SEM ACOMPANHANTE");
+} else {
+  console.log ("NÃO PODE ENTRAR ");
+}
+
+
+// Operações matemáticas
+
+//let x = 13
+//let y = 13
+//let resultado = (x + y)* 5 +(x/x)- 10;
+//resultado /= 4;
+//resultado = 3 % 20;
+//resultado = (x + y)* 5 +(x/x)- 10
+//console.log(resultado);
+
+// 
